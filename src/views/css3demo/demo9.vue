6c372ed51e45ee8fb9d4b6d6ce1f2dc962a4248b
林佳涛 <lne128gene980@163.com>
2021-05-31 17:21:57 +0800

    添加一个液体滴落的demo

---
+++ src/views/css3demo/demo9.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="warpper">
+    <div class="paint">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.warpper {
+  background-color: #000;
+  filter: contrast(30);
+  .paint {
+    width: 200px;
+    height: 400px;
+    display: flex;
+    position: relative;
+    // overflow: hidden;
+    .circle {
+      border-radius: 50%;
+      background-color: white;
+      position: absolute;
+      left: 50%;
+      top: 0;
+      filter: blur(10px);
+      &:nth-child(1) {
+        width: 60px;
+        height: 60px;
+        animation: drop 1s ease-in infinite;
+      }
+      &:nth-child(2) {
+        width: 160px;
+        height: 160px;
+        transform: translate(-50%, -50%);
+      }
+    }
+  }
+}
+
+@keyframes drop {
+  0% {
+    transform: translate(-50%, 0);
+  }
+  100% {
+    transform: translate(-50%, 400px) scale(0.5);
+  }
+}
+</style>
